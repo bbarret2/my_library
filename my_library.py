52f26929c9c18b626ca435049cb98ddb294605c8
bbarret2 <75331781+bbarret2@users.noreply.github.com>
2020-12-03 10:10:58 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,57 +1,67 @@
 import pandas as pd
+import pandas as pd
+import numpy as np
+import sys
+import os
+import math
 
+#bring in spacy. Note might want to comment this out if don't like wait time.
+os.system('python -m spacy download en_core_web_md')
+import en_core_web_md
+nlp = en_core_web_md.load()
 
+#bring in uo-puddles
+my_github_name = 'uo-puddles'
+my_library_name = 'uo_puddles'
+clone_url = f'git clone https://github.com/{my_github_name}/{my_library_name}.git'  #create the url to get the library
+os.system(clone_url) # Cloning
+import uo_puddles.uo_puddles as up
 
+#put your functions below
 def hello():
   print('hello')
 
-def dead_week(number):
-  return number
-
 def process_bio(bio):
-  good_words = []
   doc = nlp(bio)
+  good_words = []
+
   for i in range(len(doc)):
     token = doc[i]
+
     if token.is_alpha and not token.is_oov and not token.is_stop:
+      #looks good, add to good_tokens
       good_words += [token.text]
+
   return good_words
 
-def all_bayes(training_table, word_bag, bio):
-  all_classes = word_bag.columns.to_list()  #does not include word column because it is index
-  n = len(all_classes)
-  results = []
-  for i in range(n):
-    c = all_classes[i]  #a class, e.g., 'C1'
-    result = naive_bayes(training_table, word_bag, bio, c)
-    results += [[result,c]]
-  return sorted(results, reverse=True)
+def class_probability(training_table, a_class):
+  class_list = training_table['Class'].to_list()  #the Class column as a list
+  class_count = class_list.count(a_class)
+  return class_count/len(class_list)
 
-def word_by_class_probability(training_table, word_bag, word, a_class, laplace=1):
+def word_by_class_probability(training_table, word_bag, word, a_class, laplace=.1):
   class_list = training_table['Class'].to_list()
   d = len(set(class_list))
   class_count = class_list.count(a_class)  #number of bios of a_class
   word_count = word_bag.loc[word, a_class] if word in word_bag.index else 0 #bios of a_class that used the word
-  return (word_count+laplace)/(class_count + d*laplace)
+  return (word_count + laplace)/(class_count + laplace*d)
 
 def naive_bayes(training_table, word_bag, bio, a_class):
   good_words = process_bio(bio)
   n = len(good_words)
-  p_o=class_probability(training_table, a_class)
-  numerator_list =[p_o]
-  for i in range(n):
-    word=good_words[i]
-    p_word_class = word_by_class_probability(training_table,word_bag,word,a_class)
-    numerator_list += [p_word_class]
 
-  numerator = 1
-  for number in numerator_list:
-    numerator*= number
-  import math
+  numerator_list = [class_probability(training_table, a_class)]  #start if off with P(O)
+  for i in range(n):
+    word = good_words[i]
+    word_class = word_by_class_probability(training_table, word_bag, word, a_class)
+    numerator_list += [word_class]
+
   numerator = 0
   for number in numerator_list:
     numerator += math.log(number)
-  return(numerator)
+
+  return numerator
+
 
 def all_bayes(training_table, word_bag, bio):
   all_classes = word_bag.columns.to_list()  #does not include word column because it is index
@@ -61,3 +71,4 @@ def all_bayes(training_table, word_bag, bio):
     result = naive_bayes(training_table, word_bag, bio, c)
     results += [[result,c]]
   return sorted(results, reverse=True)
+
